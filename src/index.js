525c90e20832329a112d8f68f9f6853bfb565169
Serg696 <88836338+Serg696@users.noreply.github.com>
2022-02-01 15:33:11 +0300

    Change src/index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  if(matrix.length === 0) return [];
+  //let arr = matrix.map((item, index)=> item);
+  matrix.forEach((item, index) => {
+        if (index % 2 == 0) {
+          item.sort((a,b)=> a-b);
+        } else {
+          item.sort((a,b)=> b-a);
+        }
+  });
+  return matrix.join(",").split(",").map(item=> item*1);
 }
